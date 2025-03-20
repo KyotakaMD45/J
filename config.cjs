23876c78c7aa475d421a2b598f5262b5c9f60acf
KyotakaMD45 <daemonabraxas08@gmail.com>
2025-03-20 13:14:05 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "KHAN-MD~KvAA0S6Z#Enke5CZbp7WI53Q6-cDzlSFfTlidj5bVK3873C_Shv4",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
